7ab60b33444fe580c47eb9355a4267852affd75d
muborizMirzoev <muboriz10@gmail.com>
2020-04-13 12:54:52 +0700

    feat: add other function

--- app.js
+++ app.js
@@ -1,35 +1,93 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const scoreDisplay = document.querySelector('span');
+  const startBtn = document.querySelector('.start');
 
-    const squares = document.querySelectorAll('.grid div');
-    const score = document.querySelector('span');
-    const start = document.querySelector('.start');
-
-    const width = 10;    
-    let currentIndex = 0;  //first div in our grid
-    let foodIndex = 0; //first div in our grid
-    let currentSnake = [2,1,0];  //телло змеи 
-    let direction = 1;
-    let score = 0;
-    let speed - 0.9;
-    let intervalTime = 0;
-    let interval = 0;
+  const width = 10;
+  let currentIndex = 0; //first div in our grid
+  let appleIndex = 0; //first div in our grid
+  let currentSnake = [2,1,0]; // body snake
+  let direction = 1;
+  let score = 0;
+  let speed = 0.9;
+  let intervalTime = 0;
+  let interval = 0;
 
 
-    //to start, and restart the game
+  //to start, and restart the game
   function startGame() {
-    currentSnake.forEach(index => squares[index].classList.remove('snake'))
-    squares[appleIndex].classList.remove('apple')
-    clearInterval(interval)
-    score = 0
-    randomApple()
-    direction = 1
-    scoreDisplay.innerText = score
-    intervalTime = 1000
-    currentSnake = [2,1,0]
-    currentIndex = 0
-    currentSnake.forEach(index => squares[index].classList.add('snake'))
-    interval = setInterval(moveOutcomes, intervalTime)
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(interval);
+    score = 0;
+    randomApple();
+    direction = 1;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
+    currentSnake = [2,1,0];
+    currentIndex = 0;
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    interval = setInterval(moveOutcomes, intervalTime);
   }
 
+
+  
+  function moveOutcomes() {
+
     
-});
+    if (
+      (currentSnake[0] + width >= (width * width) && direction === width ) || // bottom
+      (currentSnake[0] % width === width -1 && direction === 1) || //right
+      (currentSnake[0] % width === 0 && direction === -1) || // left 
+      (currentSnake[0] - width < 0 && direction === -width) ||  //top
+      squares[currentSnake[0] + direction].classList.contains('snake') //itself
+    ) {
+      return clearInterval(interval); 
+    }
+
+    const tail = currentSnake.pop(); 
+    squares[tail].classList.remove('snake');
+    currentSnake.unshift(currentSnake[0] + direction);
+
+ 
+    if(squares[currentSnake[0]].classList.contains('apple')) {
+      squares[currentSnake[0]].classList.remove('apple');
+      squares[tail].classList.add('snake');
+      currentSnake.push(tail);
+      randomApple();
+      score++;
+      scoreDisplay.textContent = score;
+      clearInterval(interval);
+      intervalTime = intervalTime * speed;
+      interval = setInterval(moveOutcomes, intervalTime);
+    }
+    squares[currentSnake[0]].classList.add('snake');
+  }
+
+
+
+  function randomApple() {
+    do{
+      appleIndex = Math.floor(Math.random() * squares.length);
+    } while(squares[appleIndex].classList.contains('snake')); 
+    squares[appleIndex].classList.add('apple');
+  }
+
+
+  function control(e) {
+    squares[currentIndex].classList.remove('snake'); //removing the class of snake from ALL the squares.
+
+    if(e.keyCode === 39) {
+      direction = 1 //right 
+    } else if (e.keyCode === 38) {
+      direction = -width // go up
+    } else if (e.keyCode === 37) {
+      direction = -1 //  left
+    } else if (e.keyCode === 40) {
+      direction = +width // down
+    }
+  }
+
+  document.addEventListener('keyup', control);
+  startBtn.addEventListener('click', startGame);
+})
